792324ff6b5e58119b84056a7bebf0e458e7287c
Kaushik Ladumor <kaushikladumor80@gmail.com>
2026-02-11 17:29:22 +0530

    fix cors error

--- Backend/Index.js
+++ Backend/Index.js
@@ -36,10 +36,16 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://wonder-cart-three.vercel.app",
+      "https://wonder-cart-p6ep8tntl-kaushik-ladumors-projects.vercel.app"
+    ],
     credentials: true,
-  },
+    methods: ["GET", "POST"]
+  }
 });
+
 global.io = io;
 initSocket(io);
 server.listen(PORT, () => {
