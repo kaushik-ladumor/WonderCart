c9fadb0d362a99427fc634f1e300acf1a23fb691
Kaushik Ladumor <kaushikladumor80@gmail.com>
2026-04-08 23:44:55 +0530

    Fix cors policy

--- Backend/Index.js
+++ Backend/Index.js
@@ -30,6 +30,7 @@ const corsOptions = {
     "http://localhost:5176",
     "https://wondercart-customer.netlify.app",
     "https://wondercart-seller.netlify.app",
+    "https://wondercart-admin.netlify.app",
     "https://wonder-cart-three.vercel.app",
     "https://wonder-cart-p6ep8tntl-kaushik-ladumors-projects.vercel.app",
     "https://wonder-cart-gc1pbvdds-kaushik-ladumors-projects.vercel.app",
